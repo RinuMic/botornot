7c5d17653ab6f4db2e42d5e49bd7db83291f8291
RinuMic <rinumichael94@gmail.com>
2024-07-16 22:59:26 +0200

    specified port in deploy.py

--- src/deploy.py
+++ src/deploy.py
@@ -192,4 +192,5 @@ def predict():
         return jsonify({'error': 'Unexpected error occurred'}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
